37b5a8cb1062b759d5a0020a0565c8ee5c6aff0d
JosipLovric <92426389+JosipLovric@users.noreply.github.com>
2021-12-15 10:06:19 +0100

    Update main.c

--- zadatak7/main.c
+++ zadatak7/main.c
@@ -15,29 +15,11 @@ int main() {
 	
 	head.next = NULL;
 	head._dir = NULL;
-	//push(&head, napravi_stog_element(&root));
+	
 
-	//root.dite =napravi_dir("jakov");
-	//root.dite->brat = napravi_dir("josip");
-	//q = napravi_dir("zarko");
-	//root.dite = umetni(root.dite, q); //md
-	//p = cd_nar(&root, "josip");
-
-	//printf("\n\n ispis cd: %s", p->ime);
-	//dir_nar(&root);
 
 	izbornik(&root, &head);
-	/*p = napravi_stog_element(q);
-
-	push(&head, p);
-
-	q = napravi_dir("jakov");
-	p = napravi_stog_element(q);
-	push(&head, p);
-	r = pop(&head);
-
-	printf("\n jao::::::: %s", r->ime);
-*/
+	
 
 
 	return 0;
